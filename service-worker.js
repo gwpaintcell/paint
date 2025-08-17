8b04ffd9d9f04ffd7f74dacb0f51297ca945252d
gwpaintcell <markrawson93@hotmail.com>
2025-08-17 06:32:20 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,24 @@
-const CACHE_NAME = 'prodezy-cache-v1';
+const CACHE_NAME = 'calc-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+  '/style.css',
+  '/script.js',
   '/manifest.json',
-  // Add CSS, JS, images, icons here
-  // '/style.css',
-  // '/script.js',
-  // '/icon.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Install the service worker and cache files
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// Activate the service worker and remove old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) return caches.delete(cache);
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Intercept fetch requests
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
